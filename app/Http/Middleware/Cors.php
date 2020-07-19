220510496daf8a97025c82b479595dad94c09172
oirase <cashewnutmuffin@outlook.com>
2020-07-19 16:21:24 +0900

    fix_Access-Control-Allow-origin_in_middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,8 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-origin', 'https://bookshelf-booksearch.herokuapp.com/user')
         ->header('Access-Control-Allow-origin', 'https://bookshelf-booksearch.herokuapp.com')
-        ->header('Access-Control-Allow-origin', 'https://bookshelf-booksearch.herokuapp.com/');
+        ->header('Access-Control-Allow-origin', 'https://bookshelf-booksearch.herokuapp.com/')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
