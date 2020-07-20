275aa880c4a00a456b542483c20b1459ca2e88c1
oirase <cashewnutmuffin@outlook.com>
2020-07-21 01:08:32 +0900

    new

--- app/Http/Controllers/UserBookController.php
+++ app/Http/Controllers/UserBookController.php
@@ -19,22 +19,23 @@ class UserBookController extends Controller
     public function __construct(Request $request) {
 
 
-        /*
-        $this->middleware(function ($request, $next){
-            $this->userId = \Auth::user()->user_id;
-            return $next($request);
-        });
-        */
+        if(Auth::check()) {
+            $this->middleware(function ($request, $next){
+                $this->userId = \Auth::user()->user_id;
+                return $next($request);
+            });
+        }
+        
         //$this->userId = MemberInfo::value('user_Id');
         //$this->userId = '4ad49627-c212-456f-bd20-e474d4b20897';
-        //$this->limit = 20;
+        $this->limit = 20;
     }
 
     public function getUserBookList(Request $request)
     {
 
-        return 'getUserBookList';
-        /*
+        //return 'getUserBookList';
+        
         $SelectPage = $request->selectPage;
         $limit =  $this->limit;
         $offset = ($SelectPage - 1) * $limit;
@@ -69,7 +70,7 @@ class UserBookController extends Controller
         $result["totalItems"] = count($bookId);
         
         return $result;
-        */
+        
 
     }
 
