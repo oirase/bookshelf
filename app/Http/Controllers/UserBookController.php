f0e569620210c0bd4fc043099bdb6d49f14fbd6e
oirase <cashewnutmuffin@outlook.com>
2020-07-20 12:09:33 +0900

    set_userId

--- app/Http/Controllers/UserBookController.php
+++ app/Http/Controllers/UserBookController.php
@@ -35,6 +35,8 @@ class UserBookController extends Controller
     public function getUserBookList(Request $request)
     {
         
+        return $this->uerId;
+
         $SelectPage = $request->selectPage;
         $limit =  $this->limit;
         $offset = ($SelectPage - 1) * $limit;
