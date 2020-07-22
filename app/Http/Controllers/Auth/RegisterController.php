e4082905878ba5407bc4388e45ee5b76ea9bab2f
oirase <cashewnutmuffin@outlook.com>
2020-07-22 15:40:01 +0900

    app

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'user_id' => Str::uuid()
+            'user_id' => 'a1'
         ]);
     }
 }
