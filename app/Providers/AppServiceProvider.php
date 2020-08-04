ce179b1f7b5146efe20575cbe569aa10f6bf0fab
oirase <cashewnutmuffin@outlook.com>
2020-08-05 01:41:38 +0900

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,10 +23,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //if (in_array(config('app.env'),['prd', 'stg'], true)){
-            \URL::forceShema('https');
+            $url->forceShema('https');
         //}
         /*
         if(request()->isSecure()) {
